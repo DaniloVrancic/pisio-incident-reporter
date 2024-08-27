8ffa31e62644ef1b73ee53e60c91f3db23d89308
DaniloVrancic <danilov98@hotmail.com>
2024-08-27 17:31:39 +0200

    IR: Created Springboot application for backend app

---
+++ incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/IncidentReporterBackendAppApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.incident_reporter_backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentReporterBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentReporterBackendAppApplication.class, args);
+	}
+
+}
