fb8a19b8404f74f891e509524c48661fed539a77
DaniloVrancic <danilov98@hotmail.com>
2024-09-25 17:51:53 +0200

    IR 1.1: Updated entities

--- incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/repositories/UserRepository.java
+++ incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package org.springframework.boot.incident_reporter_backend_app.repositories;
 
 
+import org.springframework.boot.incident_reporter_backend_app.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
