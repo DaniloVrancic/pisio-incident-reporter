94a0b443a6b841115a798a2c3264af9009bf1c12
DaniloVrancic <danilov98@hotmail.com>
2024-09-30 17:17:45 +0200

    IR 1.8.7: Catching the clicked map coordinates

--- incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
+++ incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import {  MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MapService } from './map.service';
 
 @Component({
   selector: 'app-map',
@@ -16,15 +17,28 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
+  providers: [MapService]
   
 })
-export class AppMapComponent {
+export class AppMapComponent implements OnInit{
   options: google.maps.MapOptions = {
     mapId: "1",
     center: { lat: 44.79807782849736, lng: 20.469100626055457 },
     zoom: 13,
   };
 
+  constructor(private mapService: MapService){}
 
+  ngOnInit(): void {
+      
+  }
+
+  offerAddIncident(event: google.maps.MapMouseEvent){
+    let selectedLatitude = event.latLng?.toJSON().lat;
+    let selectedLongitude = event.latLng?.toJSON().lng;
+
+    console.log("Latitude: " + selectedLatitude);
+    console.log("Longitude: " + selectedLongitude);
+  }
 
 }
