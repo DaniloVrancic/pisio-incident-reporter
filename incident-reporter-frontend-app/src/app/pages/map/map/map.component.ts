a442efcafeb3a7fa6d36539942ca3a4815aa4545
DaniloVrancic <danilov98@hotmail.com>
2024-09-30 16:36:19 +0200

    IR 1.8.5: Added buttons inside the map

--- incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
+++ incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
@@ -3,6 +3,7 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import {  MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-map',
@@ -10,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [MatCardModule,
             MatIconModule,
             MatButtonModule,
+            MatDialogModule,
             GoogleMapsModule
   ],
   templateUrl: './map.component.html',
