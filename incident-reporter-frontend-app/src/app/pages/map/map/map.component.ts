9aa45502643cbb268caaf4c313b15eb00a7d2009
DaniloVrancic <danilov98@hotmail.com>
2024-10-08 00:25:26 +0200

    IR 1.10.4: Added functions for approve and remove on marker dialogs.

--- incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
+++ incident-reporter-frontend-app/src/app/pages/map/map/map.component.ts
@@ -119,6 +119,8 @@ export class AppMapComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(){
     this.initMap();
+    window['approveIncident'] = this.approveIncident.bind(this);
+    window['rejectIncident'] = this.rejectIncident.bind(this);
   }
 
   private initMap = async () => {
@@ -159,17 +161,51 @@ export class AppMapComponent implements OnInit, AfterViewInit {
             return;
           }
           let contentString = `
+          <style>
+          .btn {
+                cursor: pointer;
+                border: 1px solid rgb(0, 0, 0);
+                font-family: "system-ui";
+                font-size: 18px;
+                color: rgb(0, 136, 255);
+                padding: 15px 30px;
+                margin: 0.3rem;
+                transition: 146ms;
+                width: 200px;
+                box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px;
+                border-radius: 20px;
+                background: rgb(255, 255, 255);
+                font-weight: 400;
+                --hover-borderSize: 3px;
+                --hover-width: 200px;
+                --hover-borderc: #0088ff;
+                }
+
+          .btn:hover{
+                color: rgb(255, 255, 255);
+                width: 200px;
+                background: rgb(0, 102, 204) none repeat scroll 0% 0% / auto padding-box border-box;
+                border-color: rgb(0, 136, 255);
+                border-style: solid;
+                }
+          </style>
           <div class='contentContainer'>
-            <div style='align-content: center'>
+            <div style='align-content: center; justify-content: center;'>
               <img src=${incident.photoUrl} style='max-width: 12rem; max-height: 12rem;' onerror="this.src='assets/images/fallback/notFound.png';"/>
             </div>
             <div class='descriptionContainer'>
-              <h3 style='text-align: center; font-size: larger'>Description</h3>
-              <div style='font-size: 0.6rem'>${incident.description}</div>
+              <h3 style='text-align: center; font-size: 1.2rem'>Description</h3>
+              <div style='font-size: 0.85rem'>${incident.description}</div>
             </div>
             <div class='timeContainer'>
-              <h3 style='text-align: center; font-size: larger'>Time of Incident</h3>
-              <div style='font-size: 0.6rem'>${incident.timeOfIncident}</div>
+              <h3 style='text-align: center; font-size: 1.2rem'>Time of Incident</h3>
+              <div style='font-size: 0.85rem'>${incident.timeOfIncident}</div>
+            </div>
+            <div class='button-container' style="padding: 1rem 0rem; display: flex; flex-direction: column; justify-content: space-around;">
+            `+
+            ((incident.status === Status.REQUESTED) ? `<button class='btn' onclick="approveIncident(${incident.id})">Approve</button>` : ``)
+             +
+              `<button class='btn' onclick="rejectIncident(${incident.id})">Remove</button>
             </div>
           </div>`;
           
@@ -178,6 +214,16 @@ export class AppMapComponent implements OnInit, AfterViewInit {
           infoWindow.open(marker.map, marker);
       });
   });
+}
+
+private approveIncident(incidentId: number) {
+  // Your logic to approve the incident
+  console.log(`Approving incident with ID: ${incidentId}`);
+}
+
+private rejectIncident(incidentId: number) {
+  // Your logic to reject/remove the incident
+  console.log(`Removing incident with ID: ${incidentId}`);
 }
 
 toggleHighlight(markerView: any, incident: any) {
@@ -273,32 +319,6 @@ toggleHighlight(markerView: any, incident: any) {
     this.selectedLongitude = dragEvent.latLng?.toJSON().lng as number;
     this.isLocationSelected = true;
     this.cdr.detectChanges(); //DETECT CHANGES ON THE UI
-  }
-
-  public async handleLoadedMarkerClick(marker: any, incident: Incident){ //DELETE THIS AFTER TESTING I DON'T NEED IT
-    console.log(marker);
-    console.log(incident);
-
-    const {Map, InfoWindow} : any = await google.maps.importLibrary("maps");
-
-    const map = new google.maps.Map(
-
-      document.getElementById("map") as HTMLElement,
-      this.options
-    );
-
-    const contentString = "<div>Ja sam taj!</div>";
-
-    let infoWindowOptions = {
-      content: contentString,
-      headerContent: 'bitch',
-      map: map,
-      position: {lng: incident.longitude, lat: incident.latitude}
-    }
-
-    let myInfoWindow = new InfoWindow(infoWindowOptions);
-
-    myInfoWindow.open();
   }
 
   public handleMarkerRightClick(event: google.maps.MapMouseEvent){
