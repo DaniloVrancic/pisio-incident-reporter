22bdf93db0b2ebfe692ee3839e949a2de8f3466f
DaniloVrancic <danilov98@hotmail.com>
2024-10-10 00:43:51 +0200

    IR 1.11.2: Updated logos, redesigned login page

--- incident-reporter-frontend-app/src/app/layouts/full/sidebar/branding.component.ts
+++ incident-reporter-frontend-app/src/app/layouts/full/sidebar/branding.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-branding',
   template: `
-    <div class="branding">
+    <div class="branding" style="display: flex; flex-direction: row; justify-content: space-between; align-items: center">
+      <img src='assets/images/logos/location.png' style="max-width: 2.5rem; max-height: 2.5rem;">
       <h2 style="text-align: center;">Incident Reporter</h2>
     </div>
   `,
 })
 export class BrandingComponent {
-  constructor() {}
+  constructor() {
+  }
 }
--- incident-reporter-frontend-app/src/app/pages/authentication/login/login.component.ts
+++ incident-reporter-frontend-app/src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class AppSideLoginComponent {
+export class AppSideLoginComponent implements OnInit{
   constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  handleSignInGoogle(){
+    console.log("google sign-in");
+  }
 }
