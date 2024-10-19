b817228118bda21e0c50a4d3f34e041b7d1aa66b
DaniloVrancic <danilov98@hotmail.com>
2024-10-19 16:38:37 +0200

    IR 1.18.2: Should be working but Bean is bugging for some reason

--- incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/controller/NLPController.java
+++ incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/controller/NLPController.java
@@ -1,19 +1,50 @@
 package org.springframework.boot.incident_reporter_backend_app.controller;
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.incident_reporter_backend_app.enums.NLPType;
+import org.springframework.boot.incident_reporter_backend_app.nlp_core.Pipeline;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/nlp")
 public class NLPController {
 
-    public List<String> doNlp(@RequestBody final String input, final NLPType type){
-        
 
-        return null;
+
+    @Autowired
+    private StanfordCoreNLP stanfordCoreNLP;
+
+
+    public NLPController(){
+        this.stanfordCoreNLP = Pipeline.getInstance();
+    }
+
+
+    public Set<String> doNlp(@RequestBody final String input, @RequestParam final NLPType type){
+
+        CoreDocument coreDocument = new CoreDocument(input);
+        stanfordCoreNLP.annotate(coreDocument);
+        List<CoreLabel> coreLabels = coreDocument.tokens();
+        return new HashSet<>(collectList(coreLabels, type));
+    }
+
+    private List<String> collectList(List<CoreLabel> coreLabels, final NLPType type){
+        return coreLabels
+                .stream()
+                .filter(coreLabel -> type.getType().equalsIgnoreCase(coreLabel.get(CoreAnnotations.NamedEntityTagAnnotation.class)))
+                .map(CoreLabel::originalText)
+                .collect(Collectors.toList());
     }
 }
---
+++ incident-reporter-backend-app/src/main/java/org/springframework/boot/incident_reporter_backend_app/nlp_core/Pipeline.java
@@ -0,0 +1,31 @@
+package org.springframework.boot.incident_reporter_backend_app.nlp_core;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
+
+public class Pipeline {
+
+    private static Properties properties;
+    private static String propertiesName = "tokenize, ssplit, pos, lemma, ner";
+
+    private static StanfordCoreNLP stanfordCoreNLP;
+
+    public Pipeline() {
+
+    }
+
+    static {
+        properties = new Properties();
+        properties.setProperty("annotators", propertiesName);
+    }
+
+    @Bean
+    public static StanfordCoreNLP getInstance(){
+        if(stanfordCoreNLP == null) {
+            stanfordCoreNLP = new StanfordCoreNLP(properties);
+        }
+        return stanfordCoreNLP;
+    }
+}
