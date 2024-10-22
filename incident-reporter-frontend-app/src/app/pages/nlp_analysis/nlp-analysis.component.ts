fd2ffeaac273d4365d3bf638335fdcd4249b6bdb
DaniloVrancic <danilov98@hotmail.com>
2024-10-22 02:52:21 +0200

    IR 1.19.3: Finished a nice part of the web page

--- incident-reporter-frontend-app/src/app/pages/nlp_analysis/nlp-analysis.component.ts
+++ incident-reporter-frontend-app/src/app/pages/nlp_analysis/nlp-analysis.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NplAnalysisService } from './npl-analysis.service';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nlp-analysis',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatExpansionModule, 
+    MatListModule],
   templateUrl: './nlp-analysis.component.html',
   styleUrl: './nlp-analysis.component.scss'
 })
 export class AppNLPAnalysisComponent implements OnInit{
+
+  expanded: string | null = null;
+  selectedIncidentSentimentId: number | null = null;
 
   public incidentSentiments: Map<string, any[]> = new Map<string, any[]>();
   
@@ -18,7 +27,10 @@ export class AppNLPAnalysisComponent implements OnInit{
     if(this.incidentSentiments.size == 0){
       this.nlpService.getSentimentsForAllIncidents().subscribe((result: Map<string, any[]>) => {
         console.log(result);
-        this.incidentSentiments = result;
+        this.incidentSentiments = new Map(Object.entries(result));
+        for(const [key, value] of this.incidentSentiments){
+          console.log(key);
+        }
       });
     }
   }
